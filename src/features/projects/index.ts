fa969eeb756b3716b09651741b34fecf22e20422
John Paul Naag <122628970+kuyajp123@users.noreply.github.com>
2026-08-03 23:45:10 +0800

    refactor(projects): expose active project module APIs

--- src/features/projects/index.ts
+++ src/features/projects/index.ts
@@ -1,5 +1,12 @@
-export { ProjectDetailsPage } from './pages/ProjectDetailsPage';
-export { ProjectOnboardingPage } from './pages/ProjectOnboardingPage';
-export { ProjectsPage } from './pages/ProjectsPage';
+export { ProjectRequiredRoute } from './components/ProjectRequiredRoute';
+export { ProjectSelector } from './components/ProjectSelector';
 export { useProjectQuery } from './hooks/use-projects';
 export type { Project } from './models/project';
+export { DashboardPage } from './pages/DashboardPage';
+export { LegacyProjectRedirect } from './pages/LegacyProjectRedirect';
+export { ProjectOnboardingPage } from './pages/ProjectOnboardingPage';
+export {
+  ActiveProjectProvider,
+  resolveInitialProjectId,
+  useActiveProject,
+} from './providers/ActiveProjectProvider';
